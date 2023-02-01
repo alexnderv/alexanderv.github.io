d7db074ffbe7d7ebe28b90374b581946a3e27a70
Alexander V <alexander.vaga@mbicycle.com>
2023-02-02 00:44:20 +0400

    fixed dummy bugs. Added MUI and @emotion. added deploy script

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import reactLogo from './assets/react.svg';
 
 import './App.css';
 
-function App() {
+function App(): JSX.Element {
   const [count, setCount] = useState(0);
 
   return (
@@ -19,7 +19,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button type="button" onClick={() => setCount((cardCount) => cardCount + 1)}>
           count is
           {' '}
           {count}
